6383ecbb6b621e7ca721506bc585bbfcb90fcd78
SRIRAM <srirammurugesan1807@gmail.com>
2025-07-24 15:45:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+
+function delayPromise(value, delay) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(value), delay);
+  });
+}
+
+function transformArray() {
+  delayPromise([1, 2, 3, 4], 3000)
+    .then((arr) => {
+      const evenNumbers = arr.filter(num => num % 2 === 0);
+      return delayPromise(evenNumbers, 1000);
+    })
+    .then((filteredArr) => {
+      output.innerText = filteredArr.join(",");
+      const multiplied = filteredArr.map(num => num * 2);
+      return delayPromise(multiplied, 2000);
+    })
+    .then((finalArr) => {
+      output.innerText = finalArr.join(",");
+    });
+}
+
+transformArray();
